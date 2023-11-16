7a0d207fb83e0a80850d4525fc7d804d3d76365d
Daniel Marcotte <daniel.p.marcotte@gmail.com>
2023-11-16 15:58:01 -0600

    Verifying new project works

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,6 +1,7 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.math.abs
 
 /**
  * Reads lines from the given input txt file.
@@ -19,3 +20,38 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+
+data class Coordinates(
+        val row: Int,
+        val col: Int
+) {
+    fun add(other: Coordinates): Coordinates {
+        return Coordinates(this.row + other.row, this.col + other.col)
+    }
+
+    fun manhattanDistance(other: Coordinates): Int {
+        return abs(this.row - other.row) + abs(this.col - other.col)
+    }
+}
+
+data class InclusiveRange(
+        val x: Int,
+        val y: Int
+) {
+    fun fullyContains(other: InclusiveRange): Boolean {
+        return this.x <= other.x && this.y >= other.y
+    }
+
+    fun overlaps(other: InclusiveRange): Boolean {
+        return (this.x >= other.x && this.x <= other.y)
+                || (this.y >= other.x && this.y <= other.y)
+                || (other.x >= this.x && other.x <= this.y)
+                || (other.y >= this.x && other.y <= this.y)
+    }
+
+    fun merge(other: InclusiveRange): InclusiveRange {
+        check(overlaps(other))
+        return InclusiveRange(minOf(x, other.x), maxOf(y, other.y))
+    }
+}
